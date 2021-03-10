66ce3b4c6d66d233d1ac0f13efb59d889c1412ee
Marianookonn <pabloocon07@gmail.com>
2021-03-09 22:15:49 -0300

    added a new dummy for join

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import './App.css';
 import {
   BrowserRouter as Router,
-  Link
+  Link,
+  Switch,
+  Route
 } from "react-router-dom";
 import React from 'react';
 import Button from '@material-ui/core/Button';
@@ -22,98 +24,104 @@ function App() {
     setOpen(false);
   };
   return (
-    <div>
-    <div className="titleTable">
-      Poker Table
+    <Router>
+      <div>
+        <div className="titleTable">
+          Poker Table
     </div>
+        <Switch>
+          <Route path='/dumb'>
+            testtext
+          </Route>
 
-    <div className='roomOptions'>
-      <Router>
-      <Button variant="contained" color="primary" style={{marginTop:30}} onClick={handleClickOpen}>
-      <Link to='#' style={{marginTop:10, fontSize:20, textDecoration:'none', color:'white'}}>Join room</Link>
-      </Button>
-      <Dialog open={open} onClose={handleClose} aria-labelledby="form-dialog-title">
-        <DialogTitle id="form-dialog-title">Join Room</DialogTitle>
-        <DialogContent>
-          <DialogContentText>
-            To enter a room, please enter the code here.
+
+        </Switch>
+
+        <div className='roomOptions'>
+          <Button variant="contained" color="primary" style={{ marginTop: 30 }} onClick={handleClickOpen}>
+            <Link to='#' style={{ marginTop: 10, fontSize: 20, textDecoration: 'none', color: 'white' }}>Join room</Link>
+          </Button>
+          <Dialog open={open} onClose={handleClose} aria-labelledby="form-dialog-title">
+            <DialogTitle id="form-dialog-title">Join Room</DialogTitle>
+            <DialogContent>
+              <DialogContentText>
+                To enter a room, please enter the code here.
           </DialogContentText>
-          <TextField
-            autoFocus
-            margin="dense"
-            id="name"
-            label="Room Code"
-            type="number"
-            fullWidth
-          />
-        </DialogContent>
-        <DialogActions>
-          <Button onClick={handleClose} color="primary">
-            Cancel
-          </Button>
-          <Button onClick={handleClose} color="primary">
-            Join
-          </Button>
-        </DialogActions>
-      </Dialog>
+              <TextField
+                autoFocus
+                margin="dense"
+                id="name"
+                label="Room Code"
+                type="number"
+                fullWidth
+              />
+            </DialogContent>
+            <DialogActions>
 
-      <Button variant="contained" color="primary" style={{marginTop:30}} onClick={handleClickOpen}>
-      <Link to='#' style={{marginTop:10, fontSize:20, textDecoration:'none', color:'white'}}>Create room</Link>
-      </Button>
-      <Dialog open={open} onClose={handleClose} aria-labelledby="form-dialog-title">
-        <DialogTitle id="form-dialog-title">Create Room</DialogTitle>
-        <DialogContent>
-          <DialogContentText>
-            To create a room, please enter the necessary information.
+
+            </DialogActions>
+          </Dialog>
+
+          <Button variant="contained" color="primary" style={{ marginTop: 30 }} onClick={handleClickOpen}>
+            <Link to='#' style={{ marginTop: 10, fontSize: 20, textDecoration: 'none', color: 'white' }}>Create room</Link>
+          </Button>
+          <Dialog open={open} onClose={handleClose} aria-labelledby="form-dialog-title">
+            <DialogTitle id="form-dialog-title">Create Room</DialogTitle>
+            <DialogContent>
+              <DialogContentText>
+                To create a room, please enter the necessary information.
           </DialogContentText>
-          <TextField
-            autoFocus
-            margin="dense"
-            id="name"
-            label="Room Name"
-            type="text"
-            fullWidth
-          />
-          <TextField
-            autoFocus
-            margin="dense"
-            id="name"
-            label="Users"
-            type="number"
-            fullWidth
-          />
-          <TextField
-            autoFocus
-            margin="dense"
-            id="name"
-            label="Duration"
-            type="number"
-            fullWidth
-          />
-          <TextField
-            autoFocus
-            margin="dense"
-            id="name"
-            label="E-Mail"
-            type="email"
-            fullWidth
-          />
-        </DialogContent>
-        <DialogActions>
-          <Button onClick={handleClose} color="primary">
-            Cancel
+              <TextField
+                autoFocus
+                margin="dense"
+                id="name"
+                label="Room Name"
+                type="text"
+                fullWidth
+              />
+              <TextField
+                autoFocus
+                margin="dense"
+                id="name"
+                label="Users"
+                type="number"
+                fullWidth
+              />
+              <TextField
+                autoFocus
+                margin="dense"
+                id="name"
+                label="Duration"
+                type="number"
+                fullWidth
+              />
+              <TextField
+                autoFocus
+                margin="dense"
+                id="name"
+                label="E-Mail"
+                type="email"
+                fullWidth
+              />
+            </DialogContent>
+            <DialogActions>
+              <Button onClick={handleClose} color="primary">
+                Cancel
           </Button>
-          <Button onClick={handleClose} color="primary">
-            Create
-          </Button>
-        </DialogActions>
-      </Dialog>
-      
-    
-      </Router>
-    </div>
+              <Link to='/dumb'>
+                <Button>
+                  Join
 
-    </div>
+            </Button>
+              </Link>
+            </DialogActions>
+          </Dialog>
+
+
+        </div>
+
+      </div>
+    </Router>
   );
 }
 
